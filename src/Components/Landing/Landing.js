47bc6df3efa60e01cc4727f74ac7a049b73dd29a
genocideleo <testforgeno1@gmail.com>
2021-07-23 17:16:20 +0800

    first

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,43 +1,124 @@
-import useStyles from "./styles";
+import useStyles from './styles';
 import Signin from '../../Svg/Singin';
-import Login from "../../Svg/Login";
-
+import Login from '../../Svg/Login';
+import React from 'react';
 function Landing() {
-    const classes = useStyles();
-    return (
-        <div className={classes.landing}>
-            <header className={classes.header}>
-                <div className={classes.nav}>
-                    <span>Zion</span>
-                    <ul>
-                        <li>Home</li>
-                        <li>Contact</li>
-                        <li>About Us</li>
-                    </ul>
-                </div>
-            </header>
-            <main className={classes.main}>
-                <div className={classes.title}>
-                    <p>Zion is a <span className={classes.platform}>platform</span> that allows you to</p>
-                    <p><span className={classes.expose}>expose</span> your work</p>
-                    <div className={classes.titleButton}>
-                        <button className={classes.button1}>
-                            <span>
-                                <Signin />
-                                Sign in
-                            </span>
-                        </button>
-                        <button className={classes.button2}>
-                            <span>
-                                <Login />
-                                Log in
-                            </span>
-                        </button>
-                    </div>
-                </div>
-            </main>
-        </div>
-    )
+  const classes = useStyles();
+
+  const test = (e) => {
+    console.log(e.target.parentNode);
+  };
+  const wRippleEffect = function (e) {
+    const butn = e.target;
+    if (butn.localName === 'button') {
+      const x = e.clientX;
+      const y = e.clientY;
+      const left = e.target.offsetLeft;
+      const top = e.target.offsetTop;
+      const xInside = x - left;
+      const yInside = y - top;
+      console.log(left, top);
+      const circle = document.createElement('span');
+      circle.classList.add(classes.circle1);
+      circle.style.top = yInside + 'px';
+      circle.style.left = xInside + 'px';
+      console.log(butn.localName);
+      butn.appendChild(circle);
+      setTimeout(() => butn.removeChild(circle), 3000);
+    } else if (butn.localName === 'span') {
+      const x = e.clientX;
+      const y = e.clientY;
+      const left = e.target.parentNode.offsetLeft;
+      const top = e.target.parentNode.offsetTop;
+      const xInside = x - left;
+      const yInside = y - top;
+      console.log(left, top);
+      const circle = document.createElement('span');
+      circle.classList.add(classes.circle1);
+      circle.style.top = yInside + 'px';
+      circle.style.left = xInside + 'px';
+      console.log(butn.localName);
+      butn.appendChild(circle);
+      setTimeout(() => butn.removeChild(circle), 3000);
+    }
+  };
+  const pRippleEffect = function (e) {
+    const butn = e.target;
+    if (butn.localName === 'button') {
+      const x = e.clientX;
+      const y = e.clientY;
+      const left = e.target.offsetLeft;
+      const top = e.target.offsetTop;
+      const xInside = x - left;
+      const yInside = y - top;
+      console.log(left, top);
+      const circle = document.createElement('span');
+      circle.classList.add(classes.circle2);
+      circle.style.top = yInside + 'px';
+      circle.style.left = xInside + 'px';
+      console.log(butn.localName);
+      butn.appendChild(circle);
+      setTimeout(() => butn.removeChild(circle), 3000);
+    } else if (butn.localName === 'span') {
+      const x = e.clientX;
+      const y = e.clientY;
+      const left = e.target.parentNode.offsetLeft;
+      const top = e.target.parentNode.offsetTop;
+      const xInside = x - left;
+      const yInside = y - top;
+      console.log(left, top);
+      const circle = document.createElement('span');
+      circle.classList.add(classes.circle2);
+      circle.style.top = yInside + 'px';
+      circle.style.left = xInside + 'px';
+      console.log(butn.localName);
+      butn.appendChild(circle);
+      setTimeout(() => butn.removeChild(circle), 3000);
+    }
+  };
+  return (
+    <React.Fragment>
+      <div className={classes.landing}>
+        <header className={classes.header}>
+          <div className={classes.nav}>
+            <span>Zion</span>
+            <ul>
+              <li>Home</li>
+              <li>Contact</li>
+              <li>About Us</li>
+            </ul>
+          </div>
+        </header>
+        <main className={classes.main}>
+          <div className={classes.title}>
+            <p>
+              Zion is a <span className={classes.platform}>platform</span> that
+              allows you to
+            </p>
+            <p>
+              <span className={classes.expose}>expose</span> your work
+            </p>
+            <div className={classes.titleButton}>
+              <button className={classes.button1} onClick={wRippleEffect}>
+                <span>
+                  <Signin />
+                  Sign in
+                </span>
+              </button>
+
+              <button className={classes.button2} onClick={pRippleEffect}>
+                <span>
+                  <Login />
+                  Log in
+                </span>
+              </button>
+            </div>
+          </div>
+        </main>
+      </div>
+    </React.Fragment>
+  );
 }
 
-export default Landing
+export default Landing;
+// butn.parentNode.insertBefore(circle);
--- src/Components/Landing/styles.js
+++ src/Components/Landing/styles.js
@@ -1,110 +1,132 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-    
+  header: {
+    height: '64px',
+    paddingTop: '15px',
+  },
 
-    header:{
-        height: '64px',
-       paddingTop: '15px',
+  nav: {
+    color: '#18191F',
+    width: '80%',
+    margin: '0 auto',
+    display: 'flex',
+    justifyContent: 'space-between',
+    fontSize: '14px',
+    fontWeight: '600',
+    '& ul': {
+      padding: 0,
+      width: '300px',
+      listStyle: 'none',
+      display: 'flex',
+      justifyContent: 'space-between',
+      '& li': {
+        cursor: 'pointer',
+      },
     },
 
+    '& span': {
+      cursor: 'pointer',
+      display: 'flex',
+      alignItems: 'center',
+    },
+  },
 
-    nav:{
-        color: '#18191F',
-        width: '80%',
+  main: {
+    margin: '0 auto',
+    width: '80%',
+  },
+
+  title: {
+    paddingTop: '50px',
+    textAlign: 'center',
+
+    '& p': {
+      fontWeight: 'bolder',
+      color: '#3F2F6A',
+      fontSize: '34px',
+      margin: '10px 0',
+    },
+  },
+
+  platform: {
+    color: '#FF6B6B',
+  },
+
+  expose: {
+    color: '#7768F2',
+  },
+
+  titleButton: {
+    marginTop: '30px',
+
+    '& button': {
+      position: 'relative',
+      overflow: 'hidden',
+      borderRadius: '48px',
+      outline: 'none',
+
+      boxSizing: 'border-box',
+      margin: '0 5px',
+      height: '48px',
+      width: '130px',
+      fontSize: '12px',
+      fontWeight: '700',
+      letterSpacing: '1px',
+      fontFamily: 'Gilroy-Bold, sans-serif',
+      lineHeight: '20px',
+
+      '& span': {
+        // position: 'relative',
+        overflow: 'visible',
         margin: '0 auto',
+        width: '60%',
         display: 'flex',
-        justifyContent: 'space-between',
-        fontSize: '14px',
-        fontWeight: '600',
-        '& ul':{
-            padding: 0,
-            width: '300px',
-            listStyle: 'none',
-            display: 'flex',
-            justifyContent: 'space-between',
-            '& li':{
-                cursor: 'pointer',
-            }
+        justifyContent: 'space-around',
+
+        '& svg': {
+          height: '20px',
+          width: '20px',
         },
-
-        '& span': {
-            cursor: 'pointer',
-            display: 'flex',
-            alignItems: 'center',
-        }
+      },
     },
+  },
+  circle1: {
+    position: 'absolute',
+    backgroundColor: '#fff',
+    width: '100px',
+    height: '100px',
+    borderRadius: '50%',
+    transform: 'translate(-50%,-50%) scale(0)',
+    animation: `$scale 0.5s ease-out`,
+  },
+  circle2: {
+    position: 'absolute',
+    backgroundColor: '#7768F2',
+    width: '100px',
+    height: '100px',
+    borderRadius: '50%',
+    transform: 'translate(-50%,-50%) scale(0)',
+    animation: `$scale 0.5s ease-out`,
+  },
+  button1: {
+    border: 0,
+    color: '#FFF',
+    backgroundColor: '#7768F2',
+    border: '2px solid #7768F2',
+  },
+  button2: {
+    color: '#7768F2',
+    backgroundColor: '#FFF',
+    border: '2px solid #7768F2',
+  },
 
-    main: {
-        margin: '0 auto',
-        width: '80%',
+  '@keyframes scale': {
+    to: {
+      transform: 'translate(-50%,-50%) scale(3)',
+      opacity: '0.1',
     },
-
-    title:{
-        paddingTop: '50px',
-        textAlign: 'center',
-
-        '& p':{
-            fontWeight: 'bolder',
-            color: '#3F2F6A',
-            fontSize: '34px',
-            margin: '10px 0',
-        }
-        
-    },
-
-    platform:{
-        color: '#FF6B6B',
-    },
-
-    expose:{
-        color: '#7768F2',
-    },
-
-    titleButton:{
-        marginTop: '30px',
-
-        '& button':{
-            boxSizing: 'border-box',
-            margin: '0 5px',
-            height: '48px',
-            width: '130px',
-            borderRadius: '48px',
-            fontSize: '12px',
-            fontWeight: '700',
-            letterSpacing: '1px',
-            fontFamily: 'Gilroy-Bold, sans-serif',
-            lineHeight: '20px',
-            cursor: 'pointer',
-            '& span':{
-                margin: '0 auto',
-                width: '60%',
-                display: 'flex',
-                justifyContent: 'space-around',
-
-                '& svg': {
-       
-                    height: '20px',
-                    width: '20px',
-                }
-            }
-        }
-    },
-
-    button1:{
-        border: 0,
-        color: '#FFF',
-        backgroundColor: '#7768F2',
-    },
-
-    button2:{
-        color: '#7768F2',
-        backgroundColor: '#FFF',
-        border: '2px solid #7768F2',
-    }
-
-
-
-  });
+  },
+});
 
 export default useStyles;
