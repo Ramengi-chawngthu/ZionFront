75741882dccc150e5d7a785aea0a70e5398f3296
unknown <Ramengi-chawngthu>
2021-07-25 21:55:58 +0530

    Add three sections on landing page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,6 +1,10 @@
 import useStyles from "./styles";
 import Signin from "../../Svg/Singin";
 import Login from "../../Svg/Login";
+import Fast from "../../Svg/Fast";
+import Simple from "../../Svg/Simple";
+import Expose from "../../Svg/Expose";
+import ExploreContent from "../../Svg/ExploreContent";
 import Button from "@material-ui/core/Button";
 
 function Landing() {
@@ -17,6 +21,7 @@ function Landing() {
           </ul>
         </div>
       </header>
+
       <main className={classes.main}>
         <div className={classes.title}>
           <p>
@@ -27,21 +32,62 @@ function Landing() {
             <span className={classes.expose}>expose</span> your work
           </p>
           <div className={classes.titleButton}>
-            <Button variant="contained" color="primary">
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.titleBtn1}
+            >
               <Signin />
               Sign in
             </Button>
             <Button
               variant="outlined"
               color="primary"
-              style={{ border: "2px solid #7768F2" }}
+              className={classes.titleBtn2}
             >
               <Login />
               Log in
             </Button>
           </div>
         </div>
+        <div className={classes.image}></div> {/*Just a placeholder for now*/}
+        <div className={classes.exploreContent}>
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.exploreBtn}
+          >
+            Explore Content
+            <ExploreContent />
+          </Button>
+        </div>
+        <div className={classes.cards}>
+          <div className={classes.card1}>
+            <Fast />
+            <p>Fast</p>
+            <p>No more time wasting</p>
+            <p>Easily find services</p>
+          </div>
+
+          <div className={classes.card2}>
+            <Simple />
+            <p>Simple</p>
+            <p>No complication</p>
+            <p>Everything is Peer to Peer(P2P)</p>
+          </div>
+
+          <div className={classes.card3}>
+            <Expose />
+            <p>Expose</p>
+            <p>Show your work to others</p>
+            <p>Gain recognition</p>
+          </div>
+        </div>
       </main>
+
+      <footer className={classes.footer}>
+        <p>&#169; 2021 Zion. All rights reserved</p>
+      </footer>
     </div>
   );
 }
--- src/Components/Landing/styles.js
+++ src/Components/Landing/styles.js
@@ -1,85 +1,210 @@
 import { makeStyles } from "@material-ui/core/styles";
+import Smoke from "../../Images/Smoke.png";
 
-const useStyles = makeStyles((theme) => ({
-  header: {
-    height: "64px",
-    paddingTop: "15px",
-  },
+const useStyles = makeStyles(
+  (theme) => ({
+    header: {
+      height: 64,
+      paddingTop: 15,
+    },
 
-  nav: {
-    color: "#18191F",
-    width: "80%",
-    margin: "0 auto",
-    display: "flex",
-    justifyContent: "space-between",
-    fontSize: "14px",
-    fontWeight: "600",
-    "& ul": {
-      padding: 0,
-      width: "300px",
-      listStyle: "none",
+    nav: {
+      color: "#18191F",
+      width: "80%",
+      margin: "0 auto",
       display: "flex",
       justifyContent: "space-between",
-      "& li": {
+      fontSize: 14,
+      fontWeight: "600",
+      "& ul": {
+        padding: 0,
+        width: 300,
+        listStyle: "none",
+        display: "flex",
+        justifyContent: "space-between",
+        "& li": {
+          cursor: "pointer",
+        },
+      },
+
+      "& span": {
         cursor: "pointer",
+        display: "flex",
+        alignItems: "center",
       },
     },
 
-    "& span": {
-      cursor: "pointer",
+    main: {
+      margin: "0 auto",
+      width: "80%",
+    },
+
+    title: {
+      paddingTop: 50,
+      textAlign: "center",
+
+      "& p": {
+        fontWeight: "bolder",
+        color: "#3F2F6A",
+        fontSize: 34,
+        margin: "10px 0",
+      },
+    },
+
+    platform: {
+      color: "#FF6B6B",
+    },
+
+    expose: {
+      color: "#7768F2",
+    },
+
+    titleButton: {
+      marginTop: 30,
+
+      "& button": {
+        textTransform: "none",
+        boxSizing: "border-box",
+        margin: "0 5px",
+        height: 48,
+        width: 130,
+        borderRadius: 48,
+        fontSize: 12,
+        letterSpacing: 1,
+        fontFamily: "Gilroy-Bold, sans-serif",
+        lineHeight: 20,
+        cursor: "pointer",
+
+        "& svg": {
+          height: 20,
+          width: 20,
+          marginRight: 5,
+        },
+      },
+    },
+
+    titleBtn1: {
+      "&:hover": {
+        backgroundColor: theme.palette.primary.main,
+      },
+    },
+
+    titleBtn2: {
+      border: "2px solid #7768F2",
+      "&:hover": {
+        backgroundColor: "#FFF",
+        border: "2px solid #7768F2",
+      },
+    },
+
+    image: {
+      margin: "0 auto",
+      marginTop: 100,
+      backgroundImage: `url(${Smoke})`,
+      backgroundSize: "contain",
+      backgroundRepeat: "no-repeat",
+      width: 300,
+      height: 300,
+    },
+
+    exploreContent: {
       display: "flex",
-      alignItems: "center",
+      justifyContent: "center",
+      margin: "0 auto",
+      padding: "50px 0",
     },
-  },
 
-  main: {
-    margin: "0 auto",
-    width: "80%",
-  },
-
-  title: {
-    paddingTop: "50px",
-    textAlign: "center",
-
-    "& p": {
-      fontWeight: "bolder",
-      color: "#3F2F6A",
-      fontSize: "34px",
-      margin: "10px 0",
-    },
-  },
-
-  platform: {
-    color: "#FF6B6B",
-  },
-
-  expose: {
-    color: "#7768F2",
-  },
-
-  titleButton: {
-    marginTop: "30px",
-
-    "& button": {
+    exploreBtn: {
       textTransform: "none",
       boxSizing: "border-box",
       margin: "0 5px",
-      height: "48px",
-      width: "130px",
-      borderRadius: "48px",
-      fontSize: "12px",
-      fontWeight: "700",
-      letterSpacing: "1px",
-      fontFamily: "Gilroy-Bold, sans-serif",
-      lineHeight: "20px",
+      height: 52,
+      width: 200,
+      borderRadius: 45,
+      fontSize: 13.5,
+      letterSpacing: 0.5,
+      fontFamily: "Gilroy-Medium, sans-serif",
+      lineHeight: 20,
       cursor: "pointer",
+      color: "#FFF",
+
       "& svg": {
-        height: "20px",
-        width: "20px",
-        marginRight: "5px",
+        height: 20,
+        width: 20,
+        marginLeft: 10,
+      },
+
+      "&:hover": {
+        backgroundColor: theme.palette.secondary.main,
       },
     },
-  },
-}));
+
+    cards: {
+      padding: "100px 0 200px 0",
+      display: "flex",
+      justifyContent: "space-evenly",
+      "& div": {
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        color: "#FFF",
+        margin: 3,
+        width: 280,
+        height: 250,
+        borderRadius: 16,
+      },
+
+      "& svg": {
+        margin: "0 auto",
+        width: 90,
+        height: 90,
+      },
+
+      "& p:nth-of-type(1)": {
+        marginTop: 0,
+        textTransform: "uppercase",
+        fontSize: "19px",
+      },
+
+      "& p:nth-of-type(2), p:nth-of-type(3)": {
+        fontFamily: "Gilroy-Medium, sans-serif",
+        fontSize: "14px",
+      },
+
+      "& p:nth-of-type(2)": {
+        marginBottom: 0,
+      },
+
+      "& p:nth-of-type(3)": {
+        marginTop: 0,
+      },
+    },
+
+    card1: {
+      backgroundColor: "#8B81FF",
+    },
+
+    card2: {
+      backgroundColor: "#E580E4",
+    },
+
+    card3: {
+      backgroundColor: "#FF6B6B",
+    },
+
+    footer: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "#FFF",
+      height: 58,
+      width: "100%",
+      backgroundColor: "#352957",
+      fontSize: "10px",
+    },
+  }),
+  { index: 1 }
+);
 
 export default useStyles;
