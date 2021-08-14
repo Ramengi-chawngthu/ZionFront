e159b450ea7a1c831c309853be5a223f0862ec6b
genocideleo <testforgeno1@gmail.com>
2021-08-15 05:32:19 +0800

    fifthteenAug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import "./App.css";
-import { ThemeProvider } from "@material-ui/core/styles";
-import Theme from "./Components/Styles/Theme";
-import Home from "./Components/Home/Home";
-import Landing from "./Components/Landing/Landing";
+import './App.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Theme from './Components/Styles/Theme';
+import Home from './Components/Home/Home';
+import Landing from './Components/Landing/Landing';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={Theme}>
-        <Home />
+        <Landing />
       </ThemeProvider>
     </>
   );
